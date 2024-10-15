78ba6811856eb826d6a36de716ac45960f5e2a2d
MowlandCodes <mowlandgaming@gmail.com>
2024-10-15 09:11:00 +0700

    Initial Commit

--- mSpy.py
+++ mSpy.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+import pyfiglet
+import subprocess
+
+
+def check_root() -> bool:
+    return subprocess.run("id -u", shell=True, capture_output=True) == 0
+
+
+
+if __name__ == '__main__':
+    if check_root() == False:
+        print(pyfiglet.figlet_format("Need root!", font="ANSI Shadow"))
+        exit(1)
+
+
